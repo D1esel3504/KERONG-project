6194f694ec5805c2ab284c13e71a073a9404cce7
D1esel3504 <davidovich94@mail.ru>
2022-05-13 15:54:17 +0300

    authorization

--- src/modules/LoginForm/LoginForm.jsx
+++ src/modules/LoginForm/LoginForm.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './LoginForm.scss';
 
 const LoginForm = () => {
+  let history = useNavigate();
+  let [userEmail, setEmail] = useState('');
+  let [userPassword, setPassword] = useState('');
+
+  // 'kemalkalandarov@gmail.com'
+  // 'test123'
+
+  let userData = {
+    email: userEmail,
+    password: userPassword,
+  }
+
+  let handleLogIn = (e) => {
+    e.preventDefault();
+
+    try {
+      fetch('https://tms-js-pro-back-end.herokuapp.com/api/users/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      }).then((response) => {
+        if (response.status === 200) {
+          history('/')
+        }
+        
+      })
+    } catch (error) {
+      console.log('SERVER ERROR')
+    }
+  }
 
   return (
     <div>
-      <form className='form'>
-        <input type='text' placeholder='ENTER EMAIL' />
-        <input type='password' placeholder='ENTER PASSWORD' />
-        <button type="submit">LOG IN</button>
+      <form className='form' onSubmit={handleLogIn}>
+        <input onChange={(e) => setEmail(e.target.value)} type='text' placeholder='ENTER EMAIL' />
+        <input onChange={(e) => setPassword(e.target.value)} type='password' placeholder='ENTER PASSWORD' />
+        <button type='submit'>LOG IN</button>
       </form>
     </div>
   );
