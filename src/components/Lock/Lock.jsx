70fb544c33d2dd5025b8a1fd67edd3d3b524bc41
D1esel3504 <davidovich94@mail.ru>
2022-05-17 14:31:53 +0300

    change local state

--- src/components/Lock/Lock.jsx
+++ src/components/Lock/Lock.jsx
@@ -1,21 +1,16 @@
 import React, { useState } from 'react'
-import './Lock.scss';
-import { Button, Input, Typography, Tooltip, Alert } from 'antd';
-import { UnlockFilled, EditFilled, SaveFilled, CloseCircleFilled } from '@ant-design/icons';
+import './Lock.scss'
+import { Button, Input, Typography, Tooltip } from 'antd'
+import {
+    UnlockFilled,
+    EditFilled,
+    SaveFilled,
+    CloseCircleFilled,
+} from '@ant-design/icons'
 
-const Lock = ({ lock, setLock }) => {
-    let [commentInputComponent, setCommentInputComponent] = useState('');
-    let [isShowEditInput, setisShowEditInput] = useState(false);
-
-    let dataForChangeStatus = {
-        ...lock,
-        state: 'opened',
-    };
-
-    let dataForChangeComment = {
-        ...lock,
-        comment: commentInputComponent,
-    };
+const Lock = ({ lock, setLock, index }) => {
+    let [commentInputComponent, setCommentInputComponent] = useState('')
+    let [isShowEditInput, setisShowEditInput] = useState(false)
 
     // let requestForLockonServer = (lockNumber, data) => {
     //     return fetch(`https://tms-js-pro-back-end.herokuapp.com/api/meet-rooms/${lockNumber}`,
@@ -24,7 +19,7 @@ const Lock = ({ lock, setLock }) => {
     //             body: JSON.stringify(data),
     //             headers: {
     //                 'Content-Type': 'application/json',
-    //                 Authorization: `Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtlbWFsa2FsYW5kYXJvdkBnbWFpbC5jb20iLCJpZCI6IjYxMDJiOWMxMmFhYTkwMGMwZTI2OGFkZSIsImV4cCI6MTY1NzMxNTM1NSwiaWF0IjoxNjUyMTMxMzU1fQ.c62OU82mCuRHxOZDMDyXaKhm3LYXS7kvywUXXfiMH2M`,
+    //                 Authorization: Token `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtlbWFsa2FsYW5kYXJvdkBnbWFpbC5jb20iLCJpZCI6IjYxMDJiOWMxMmFhYTkwMGMwZTI2OGFkZSIsImV4cCI6MTY1NzMxNTM1NSwiaWF0IjoxNjUyMTMxMzU1fQ.c62OU82mCuRHxOZDMDyXaKhm3LYXS7kvywUXXfiMH2M`,
     //             },
     //         });
     // };
@@ -39,12 +34,7 @@ const Lock = ({ lock, setLock }) => {
     //         setLock(json);
 
     //     } catch (error) {
-    //         <Alert
-    //         message="Error"
-    //         description={error}
-    //         type="error"
-    //         showIcon
-    //     />
+    //         console.error('error-' + error);
     //     };
     // };
 
@@ -60,48 +50,43 @@ const Lock = ({ lock, setLock }) => {
     //         setisShowEditInput(false);
 
     //     } catch (error) {
-    //         <Alert
-    //         message="Error"
-    //         description={error}
-    //         type="error"
-    //         showIcon
-    //     />
+    //         console.error('error-' + error);
     //     };
     // };
 
-    let changeCommentInLocalState = () => {
-        setLock(dataForChangeComment);
-
-        setisShowEditInput(false);
-    }
-
-    let changeStatusInLocalState = () => {
-        setLock(dataForChangeStatus)
+    let changeLockInLocalState = (index, lockNumber, data) => {
+        setLock(index, lockNumber, data)
+        setisShowEditInput(false)
     }
 
     return (
         <>
             {lock && (
-                <div className='lock-block'>
-                    <Typography.Title style={{ margin: 0 }} level={5}> NUMBER - {lock.lockNumber}</Typography.Title>
+                <div className="lock-block">
+                    <Typography.Title style={{ margin: 0 }} level={5}>
+                        NUMBER - {lock.lockNumber}
+                    </Typography.Title>
                     {isShowEditInput ? (
-                        <div className='lock-description'>
+                        <div className="lock-description">
                             <Input
-                                placeholder='ENTER THE COMMENT'
+                                placeholder="ENTER THE COMMENT"
                                 allowClear
                                 onChange={(e) => setCommentInputComponent(e.target.value)}
                                 size="small"
                             />
-
-                            <div className='lock-description__buttons'>
+                            <div className="lock-description__buttons">
                                 <Tooltip title="save comment">
                                     <Button
                                         size="large"
                                         type="primary"
-                                        onClick={changeCommentInLocalState}
+                                        onClick={() =>
+                                            changeLockInLocalState(index, lock.lockNumber, {
+                                                ...lock,
+                                                comment: commentInputComponent,
+                                            })}
                                         icon={<SaveFilled />}
-                                        danger>
-                                    </Button>
+                                        danger
+                                    ></Button>
                                 </Tooltip>
                                 <Tooltip title="close">
                                     <Button
@@ -111,40 +96,47 @@ const Lock = ({ lock, setLock }) => {
                                         icon={<CloseCircleFilled />}
                                         danger
                                         style={{
-                                            marginLeft: '5px'
-                                        }}>
-                                    </Button>
+                                            marginLeft: '5px',
+                                        }}
+                                    ></Button>
                                 </Tooltip>
                             </div>
                         </div>
-                    ) : (<div className='lock-description'>
-                        <span> COMMENT - {lock.comment || 'empty'}</span>
-                        <Tooltip title="edit comment">
-                            <Button
-                                type="primary"
-                                size="large"
-                                onClick={() => setisShowEditInput(true)}
-                                icon={<EditFilled />}
-                                danger>
-                            </Button>
-                        </Tooltip>
-                    </div>)}
-                    <div className='lock-description'>
+                    ) : (
+                        <div className="lock-description">
+                            <span className="lock-description__comment"> COMMENT - {lock.comment || 'empty'}</span>
+                            <Tooltip title="edit comment">
+                                <Button
+                                    type="primary"
+                                    size="large"
+                                    onClick={() => setisShowEditInput(true)}
+                                    icon={<EditFilled />}
+                                    danger
+                                ></Button>
+                            </Tooltip>
+                        </div>
+                    )}
+                    <div className="lock-description">
                         <span> STATUS - {lock.state}</span>
                         <Tooltip title="open lock">
                             <Button
                                 type="primary"
                                 size="large"
                                 icon={<UnlockFilled />}
-                                onClick={changeStatusInLocalState}
-                                danger>
-                            </Button>
+                                onClick={() =>
+                                    changeLockInLocalState(index, lock.lockNumber, {
+                                        ...lock,
+                                        state: lock.state === 'closed' ? 'opened' : 'closed',
+                                    })
+                                }
+                                danger
+                            ></Button>
                         </Tooltip>
                     </div>
                 </div>
             )}
         </>
-    );
-};
+    )
+}
 
-export default Lock;
+export default Lock
--- src/modules/Boards/Boards.jsx
+++ src/modules/Boards/Boards.jsx
@@ -1,82 +1,111 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import Lock from '../../components/Lock/Lock';
-import { Context } from '../../context';
-import { Button, Input, Typography } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
-import './Boards.scss';
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Lock from '../../components/Lock/Lock'
+import { Context } from '../../context'
+import { Button, Input, Typography } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
+import './Boards.scss'
 
 const Boards = () => {
-  let [boardList, setBoardList] = useState([]);
-  let [temp, setTemp] = useState([]);
-  let [boardInput, setBoardInput] = useState('');
-  let { id } = useParams();
-  let { controllersList } = useContext(Context);
+  let [boardList, setBoardList] = useState([])
+  let [temp, setTemp] = useState([])
+  let [boardInput, setBoardInput] = useState('')
+  let { id } = useParams()
+  let { controllersList } = useContext(Context)
 
   useEffect(() => {
-    let controller = controllersList.filter(i => i.ip === id);
+    let controller = controllersList.filter((i) => i.ip === id)
 
     let boards = controller.reduce((acc, curr) => {
       return [...acc, ...curr.boards]
     }, [])
 
-    setBoardList(boards);
+    setBoardList(boards)
 
-    setTemp(boards);
-  }, []);
+    setTemp(boards)
+  }, [])
 
   let filterBoards = (numberBoard) => {
-    let tempBoards = [...boardList];
+    let tempBoards = [...boardList]
 
-    let filtredBoard = tempBoards.filter(i => i.number.toString().includes(numberBoard));
+    let filtredBoard = tempBoards.filter((i) =>
+      i.number.toString().includes(numberBoard),
+    )
 
-    setTemp(filtredBoard);
-  };
+    setTemp(filtredBoard)
+  }
+
+  let setLastModifications = (index, lockNumber, data) => {
+    let changedArr = [...temp];
+
+    let changedLock = {
+      ...changedArr[index].locks[lockNumber],
+      ...data
+    }
+
+    changedArr.splice(index, 1, {
+      ...changedArr[index],
+      locks: {
+        ...changedArr[index].locks,
+        [lockNumber]: {
+          ...changedLock
+        },
+      },
+    })
+
+    setTemp(changedArr);
+    setBoardList(changedArr)
+  }
 
   return (
     <div>
       <div className="info">
         <Typography.Title level={1}>BOARDS:</Typography.Title>
-        <div className='search'>
+        <div className="search">
           <Input
-            placeholder='THE NUMBER OF BOARD'
+            placeholder="THE NUMBER OF BOARD"
             allowClear
             onChange={(e) => setBoardInput(e.target.value)}
           />
           <Button
             style={{
-              marginLeft: '10px'
+              marginLeft: '10px',
             }}
             danger
             type="primary"
             onClick={() => filterBoards(boardInput)}
-            icon={<SearchOutlined />}>
+            icon={<SearchOutlined />}
+          >
             Search
           </Button>
         </div>
         <div className="boards">
           {temp.length
-            ? temp.map(board => (
+            ? temp.map((board, index) => (
               <div>
-                <span onClick={() => filterBoards(board.number)} className="choosed-board">
+                <span
+                  onClick={() => filterBoards(board.number)}
+                  className="choosed-board"
+                >
                   BOARD - {board.number}
                 </span>
-                {Object.keys(board.locks).map(lock => (
+                {Object.keys(board.locks).map((lock) => (
                   <Lock
                     lock={{
                       lockNumber: lock,
-                      ...board.locks[lock]
+                      ...board.locks[lock],
                     }}
+                    index={index}
+                    setLock={setLastModifications}
                   />
-                )
-                )}
+                ))}
               </div>
             ))
             : 'NO BOARDS AND LOCKS'}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Boards;
+export default Boards
