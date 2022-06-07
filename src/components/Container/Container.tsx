f968e9a5646baccaa51017e73a6f8fa22a32cc5e
D1esel3504 <davidovich94@mail.ru>
2022-06-07 16:12:31 +0300

    NotFoundPage fix

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -6,7 +6,7 @@ import logo from '../../image/logo.jpg';
 import { MOCKED_CONTROLLERS } from '../../mockedData';
 import './Container.scss';
 import { Alert } from 'antd';
-import {  IController, ILockKeys } from '../../types/types';
+import { IController, ILockKeys } from '../../types/types';
 import axios from 'axios';
 
 interface ContainerProps {
@@ -58,7 +58,9 @@ const Container: FC<ContainerProps> = ({ children }) => {
       <div className="container">
         <div className="header">
           <img className="header__logo" alt="logo" src={logo} />
-          {location.pathname !== '/login' && <CheckLocks />}
+          {location.pathname !== '/login'
+            && location.pathname === undefined
+            && <CheckLocks />}
         </div>
         <div>{children}</div>
         {isShowAlert && (
